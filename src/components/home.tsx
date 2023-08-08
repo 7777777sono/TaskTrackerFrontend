2d5695906e843983ab0c6e44c1da802fa58f006e
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2023-08-08 15:56:13 +0900

    LINEに通知を送るために必要なトークンをバックエンドに送ることに成功

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,10 +1,39 @@
 import { useIsLogin, useLoginUser } from "../context/accountManagementContext";
+import { useQuery } from "../context/queryContext";
 import Login from "./login";
 import Task from "./task";
+import { useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
   const [isLogin, setIsLogin] = useIsLogin(); // ログインしているかどうかを判別する
   const [user, setUser] = useLoginUser(); // ログインしたユーザの情報を格納するオブジェクト
+  const query = useQuery(); // クエリパラメータを格納する
+
+  useEffect(() => {
+    // ログインしたユーザの情報の取得をする関数
+    const getLoginUser = async () => {
+      let getLoginUserUrl: string =
+        "http://127.0.0.1:4000/users/" + query.userId;
+      try {
+        const response = await axios.get(getLoginUserUrl);
+        setUser(response.data);
+        setIsLogin(true);
+      } catch (error) {
+        setIsLogin(false);
+        alert("ユーザの情報取得に失敗しましたのでログアウトしました。");
+      }
+    };
+
+    // クエリパラメータが空かどうかを判別
+    const isQueryParamEmpty: boolean = Object.keys(query).length === 0;
+    if (!isQueryParamEmpty && query.isLogin === "true") {
+      getLoginUser();
+    } else {
+      setIsLogin(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return <>{isLogin ? <Task></Task> : <Login></Login>}</>;
 };
--- src/components/task.tsx
+++ src/components/task.tsx
@@ -1,11 +1,25 @@
+import Link from "next/link";
 import TaskAdd from "./taskAdd";
 import TaskList from "./taskList";
+import { useIsLogin, useLoginUser } from "../context/accountManagementContext";
 
 const Task = () => {
+  const [isLogin, setIsLogin] = useIsLogin(); // ログインしているかどうかを判別する
+  const [user, setUser] = useLoginUser(); // ログインしたユーザの情報を格納するオブジェクト
+
   return (
     <>
       <TaskAdd></TaskAdd>
       <TaskList></TaskList>
+      <Link
+        href={{
+          pathname: "/notifySettingPage",
+          query: { isLogin: isLogin, userId: user.id },
+        }}
+        legacyBehavior
+      >
+        <a>LINEの通知設定は、こちらから</a>
+      </Link>
     </>
   );
 };
