46401a981aeb3c81c9bcd025ea7fe00ea188539a
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2023-08-11 16:58:24 +0900

    ログインに関するデザイン完了

--- src/components/HeadZone.tsx
+++ src/components/HeadZone.tsx
@@ -11,6 +11,10 @@ const HeadZone = () => {
           type="image/png"
           href="/images/task_tracker_icon.png"
         />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"
+        />
       </Head>
     </>
   );
